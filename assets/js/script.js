d3fd57f971c24933446bc9964aae4ef7384d4b96
Ulugbek Osmonov <agusakurdak@gmail.com>
2024-12-05 17:57:19 +0600

    I did everything, I put my heart my sweat

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,36 @@
-const swiper = new Swiper('.swiper',  {
-    slidesPerView: 2,
+const accordeons = document.querySelectorAll('.accordeon__top');
+
+accordeons.forEach(a => {
+    a.addEventListener('click', () => {
+        const text = a.nextElementSibling; // Get the adjacent .item__text
+        if (text) {
+            text.classList.toggle('hidden'); // Toggle the 'hidden' class
+        }
+    })
+});
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
     spaceBetween: 30,
-    freeMode: true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+const aboutSlider = new Swiper('.about__slider', {
+    slidesPerView: 5,
+    spaceBetween: 30,
     loop: true,
 });
+
+const testimonialSlider = new Swiper('.testimonials__swiper', {
+    slidesPerView: 1,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
